c894dfc77fd8ca281a25793114bced27e9735e09
Sameer Ramesh biradar <biradarsameer2@gmail.com>
2024-09-25 14:52:15 +0530

    Update Hi.java

--- src/main/java/interview_Coding_Questions/Hi.java
+++ src/main/java/interview_Coding_Questions/Hi.java
@@ -3,5 +3,6 @@ package interview_Coding_Questions;
 public class Hi {
     public static void main(String[] args) {
         System.out.println("ho");//make it hi
+        System.out.println("i am in class")
     }
 }
